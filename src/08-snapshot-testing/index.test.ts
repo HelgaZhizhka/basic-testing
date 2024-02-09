6cd1b02f23623e0c8d7824c076014d2b282112c4
HelgaZhizhka <helgazhyzhka@gmail.com>
2024-02-09 23:01:30 +0100

    feat: add test for tasks: 01, 02, 03, 08

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,19 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+     const elements = [1, 2, 3];
+     const resultLinkedList = generateLinkedList(elements);
+
+     expect(resultLinkedList).toMatchSnapshot();
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+     const elements = [2, 5, 6];
+     const resultLinkedList = generateLinkedList(elements);
+     
+     expect(resultLinkedList).toMatchSnapshot();
   });
 });
