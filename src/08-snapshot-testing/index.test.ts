88ae60b0c6bd60711f169ea998bba343241baa24
HelgaZhizhka <helgazhyzhka@gmail.com>
2024-02-12 15:41:46 +0100

    fix: refactor test for task8

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,19 +1,75 @@
 import { generateLinkedList } from './index';
 
+interface TechNode {
+  value: string | null;
+  next: TechNode | null;
+}
+
 describe('generateLinkedList', () => {
+  let techList: string[];
+  let expectedTechNode: TechNode;
+
+  beforeEach(() => {
+    techList = [
+      'JavaScript',
+      'React',
+      'Vue',
+      'Node.js',
+      'Webpack',
+      'Jest',
+      'TypeScript',
+      'CSS',
+      'HTML',
+      'GraphQL',
+    ];
+
+   expectedTechNode = {
+     value: 'JavaScript',
+     next: {
+       value: 'React',
+       next: {
+         value: 'Vue',
+         next: {
+           value: 'Node.js',
+           next: {
+             value: 'Webpack',
+             next: {
+               value: 'Jest',
+               next: {
+                 value: 'TypeScript',
+                 next: {
+                   value: 'CSS',
+                   next: {
+                     value: 'HTML',
+                     next: {
+                       value: 'GraphQL',
+                       next: {
+                         value: null,
+                         next: null,
+                       },
+                     },
+                   },
+                 },
+               },
+             },
+           },
+         },
+       },
+     },
+   };
+
+
+  });
+
   // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-     const elements = [1, 2, 3];
-     const resultLinkedList = generateLinkedList(elements);
-
-     expect(resultLinkedList).toMatchSnapshot();
+    const resultTechNode = generateLinkedList(techList);
+    expect(resultTechNode).toStrictEqual(expectedTechNode);
   });
 
   // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-     const elements = [2, 5, 6];
-     const resultLinkedList = generateLinkedList(elements);
-     
-     expect(resultLinkedList).toMatchSnapshot();
+    const resultTechNode = generateLinkedList(techList);
+    expect(resultTechNode).toMatchSnapshot();
   });
 });
